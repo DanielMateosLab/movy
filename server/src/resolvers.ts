a3fba32cc7234d85a5c81b880fe68c7526dc32c4
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-14 19:01:06 +0100

    Set up graphql-codegen to get resolvers' types

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,9 +1,15 @@
-const resolvers = {
+import { Resolvers } from "./types/graphql";
+
+const enumResolvers = {
   Type: {
     MOVIE: "movie",
     SERIES: "series",
     EPISODE: "episode",
   },
 };
+
+type ExtendedResolvers = Resolvers & typeof enumResolvers;
+
+const resolvers: ExtendedResolvers = { ...enumResolvers };
 
 export default resolvers;
