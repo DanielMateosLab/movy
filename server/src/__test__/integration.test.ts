6719bc66cedfc36eab17fa896fd9530627df72fb
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-17 16:40:11 +0100

    Write tests for Complex Search By Title Query

--- server/src/__test__/integration.test.ts
+++ server/src/__test__/integration.test.ts
@@ -2,7 +2,17 @@ import { ApolloServer } from "apollo-server";
 import resolvers from "../resolvers";
 import { typeDefs } from "../schema";
 import OMDbApi from "../datasources/OMDbApi";
-import { errorMockResponse, showsByTitleMockRes } from "./mockApiResults";
+import {
+  errorMockResponse,
+  showByIdMockResponse,
+  showsByTitleMockRes,
+} from "./mockApiResults";
+import { toReadableJSON } from "./utils";
+
+/** Here resolvers and dataSources are tested in conjunction.
+ *  To do so, I mock the response from the get() method of the RESTDataSource
+ *  class with real responses of the OMDbAPI.
+ */
 
 describe("Queries", () => {
   let omdbApi: OMDbApi;
@@ -18,9 +28,11 @@ describe("Queries", () => {
     });
   });
   describe("Query.showsByTitle", () => {
-    // We define the standard operation we want to test
-    const operation = {
-      query: `
+    describe("Simple Search Show Query", () => {
+      // This query tries to fetch only for the fields we get from a normal
+      // "search by title" OMDbAPI request.
+      const operation = {
+        query: `
         query TestQuery($title: String!) {
           showsByTitle(title: $title) {
             result {
@@ -35,31 +47,79 @@ describe("Queries", () => {
             }
           }
         `,
-      variables: { title: "mock" },
-    };
+        variables: { title: "mock" },
+      };
 
-    it("gets a list of shows with a valid search string", async () => {
-      // Mock a success response from API
-      // @ts-ignore - We skip checks here because get is a protected method
-      // and we can not call .mockImplementationValueOnce() on it
-      OMDbApi.prototype.get = jest.fn(async () => showsByTitleMockRes);
+      it("gets a list of shows", async () => {
+        // Mock a success response from API
+        // @ts-ignore - We skip checks here because get is a protected method
+        // and we can not call .mockImplementationValueOnce() on it
+        OMDbApi.prototype.get = jest.fn(async () => showsByTitleMockRes);
 
-      const result = await testServer.executeOperation(operation);
+        const result = await testServer.executeOperation(operation);
 
-      expect(result.errors).toBeUndefined();
-      expect(result.data).toBeDefined();
-      expect(result).toMatchSnapshot();
+        expect(result.errors).toBeUndefined();
+        expect(result.data).toBeDefined();
+        expect(result).toMatchSnapshot();
+      });
+
+      it("returns an error if with an invalid search string", async () => {
+        // @ts-ignore
+        OMDbApi.prototype.get = jest.fn(async () => errorMockResponse);
+
+        const result = await testServer.executeOperation(operation);
+
+        expect(result.data).toBeNull();
+        expect(result.errors).toBeDefined();
+        expect(result).toMatchSnapshot();
+      });
     });
 
-    it("returns an error if with an invalid search string", async () => {
-      // @ts-ignore
-      OMDbApi.prototype.get = jest.fn(async () => errorMockResponse);
+    describe("Complex Search Query", () => {
+      // This query tries to fetch all the details of a show, so additional
+      // requests to "show by id" OMDbAPI endpoints are needed.
+      const operation = {
+        query: `
+         query TestQuery($title: String!) {
+            showsByTitle(title: $title) {
+              result {
+                id
+                type
+                title
+                poster
+                year
+                genre
+                plot
+                director
+                actors
+                writer
+                awards
+              }
+            }
+          }
+        `,
+        variables: { title: "mock" },
+      };
 
-      const result = await testServer.executeOperation(operation);
+      it("gets a list of shows", async () => {
+        // We mock .get() method so it differenciates between searchs by title or by id
+        const mockGet = jest.fn(async (_, queryParams) => {
+          // If "i" is in queryParams, we are searching by id
+          if ("i" in queryParams) return showByIdMockResponse;
+          // Else, we are searching by title
+          return showsByTitleMockRes;
+        });
 
-      expect(result.data).toBeNull();
-      expect(result.errors).toBeDefined();
-      expect(result).toMatchSnapshot();
+        // @ts-ignore
+        OMDbApi.prototype.get = mockGet;
+
+        const result = await testServer.executeOperation(operation);
+
+        console.log(toReadableJSON(result));
+
+        expect(result.errors).toBeUndefined();
+        expect(result.data).toBeDefined();
+      });
     });
   });
   describe.skip("Query.showById", () => {});
