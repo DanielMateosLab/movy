20ac79c04421483cce871932be0b08ce63875779
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-21 22:48:01 +0100

    Add search form

--- client/__test__/unit/home.test.tsx
+++ client/__test__/unit/home.test.tsx
@@ -15,4 +15,19 @@ describe("Home", () => {
     expect(heading).toBeInTheDocument();
     expect(logo).toBeInTheDocument();
   });
+  it("has a form to search by show title and type", () => {
+    render(<Home />);
+
+    const titleInput = screen.getByLabelText("Title");
+    const typeSelect = screen.getByRole("combobox", {
+      name: "Show Type",
+    });
+    const submitButton = screen.getByRole("button", {
+      name: /search/i,
+    });
+
+    expect(titleInput).toBeInTheDocument();
+    expect(typeSelect).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
 });
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import logo from "public/logo.svg";
+import { Formik, Form, Field, FieldProps } from "formik";
+import { searchShowByTitleValidation } from "utils/validation";
 
 const Home: NextPage = () => {
   return (
@@ -24,6 +26,51 @@ const Home: NextPage = () => {
             <h1 className="title">Movy</h1>
           </div>
         </header>
+
+        <Formik
+          initialValues={{
+            title: "",
+            type: "",
+            page: 1,
+          }}
+          validationSchema={searchShowByTitleValidation}
+          onSubmit={(values, { setSubmitting }) => {
+            console.log(values);
+            setSubmitting(false);
+          }}
+        >
+          <Form>
+            <div className="form">
+              <Field name="title">
+                {({ field, meta }: FieldProps) => (
+                  <div className="form-field">
+                    <label htmlFor="title">Title</label>
+                    <input
+                      type="text"
+                      id="title"
+                      {...field}
+                      placeholder="Batman Begins"
+                    />
+                    {meta.touched && meta.error && (
+                      <div className="error">{meta.error}</div>
+                    )}
+                  </div>
+                )}
+              </Field>
+              <div>
+                <label htmlFor="type">Show Type</label>
+                <Field as="select" name="type" id="type">
+                  <option value="">All</option>
+                  <option value="MOVIE">Movie</option>
+                  <option value="SERIES">Series</option>
+                  <option value="EPISODE">Episode</option>
+                </Field>
+              </div>
+
+              <button type="submit">Search</button>
+            </div>
+          </Form>
+        </Formik>
       </main>
 
       <style jsx>
@@ -32,7 +79,7 @@ const Home: NextPage = () => {
             display: flex;
             justify-content: center;
             align-items: center;
-            margin-top: 1rem;
+            margin: 1rem 0;
             gap: 2rem;
           }
           .logo {
@@ -43,6 +90,44 @@ const Home: NextPage = () => {
             margin: 0;
             text-transform: uppercase;
             letter-spacing: 2px;
+          }
+        `}
+      </style>
+
+      <style jsx global>
+        {`
+          .form {
+            display: flex;
+            justify-content: center;
+            gap: 1rem;
+          }
+          label {
+            margin-right: 0.25rem;
+          }
+          select,
+          input,
+          button {
+            font-family: "Raleway";
+            padding: 0.25rem;
+            font-size: 1rem;
+          }
+          button {
+            padding: 0.25rem 0.5rem;
+            background: rgb(255, 200, 55);
+            border: 1px solid black;
+          }
+          button:active {
+            background: rgb(184, 143, 41);
+          }
+
+          .form-field {
+            position: relative;
+          }
+          .error {
+            position: absolute;
+            font-size: 0.875rem;
+            color: rgb(185, 0, 0);
+            margin-top: 0.2rem;
           }
         `}
       </style>
