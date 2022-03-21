fad9e381513ae136913e588af19d9fd53f064c97
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-21 18:01:12 +0100

    Style brand

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -17,19 +17,32 @@ const Home: NextPage = () => {
 
       <main>
         <header>
-          <Image
-            src={logo}
-            alt="a video camera as the movy logo"
-            layout="fill"
-          />
-          <h1>Movy</h1>
+          <div className="brand">
+            <div className="logo">
+              <Image src={logo} alt="a video camera as the movy logo" />
+            </div>
+            <h1 className="title">Movy</h1>
+          </div>
         </header>
       </main>
 
       <style jsx>
         {`
-          header {
-            max-width: fit-content;
+          .brand {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-top: 1rem;
+            gap: 2rem;
+          }
+          .logo {
+            width: 15vh;
+          }
+          .title {
+            font-size: 4rem;
+            margin: 0;
+            text-transform: uppercase;
+            letter-spacing: 2px;
           }
         `}
       </style>
