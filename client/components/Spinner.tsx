2eaab0384599b434d353af54976050add0e0b1aa
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-25 17:01:52 +0100

    Create Spinner

---
+++ client/components/Spinner.tsx
@@ -0,0 +1,27 @@
+const Spinner: React.FC = () => (
+  <>
+    <div aria-label="Loading..." className="loading" />
+
+    <style jsx>
+      {`
+        .loading {
+          width: 4rem;
+          height: 4rem;
+          border: 0.25rem solid rgba(0, 0, 0, 0.3);
+          border-radius: 50%;
+
+          border-top-color: rgba(0, 0, 0, 0.9);
+          animation: spin 1s ease-in-out infinite;
+        }
+
+        @keyframes spin {
+          to {
+            transform: rotate(360deg);
+          }
+        }
+      `}
+    </style>
+  </>
+);
+
+export default Spinner;
