cc9205263de93e13be0d8e81c578b76116b988db
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-27 17:14:56 +0200

    Create ShowById query

--- client/pages/show/[id].tsx
+++ client/pages/show/[id].tsx
@@ -1,7 +1,25 @@
+import { gql } from "@apollo/client";
 import AppHeader from "components/AppHeader";
 import Layout from "components/Layout";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+
+const SHOW_BY_ID = gql`
+  query ShowById($id: ID!) {
+    showById(id: $id) {
+      poster
+      title
+      type
+      year
+      plot
+      genre
+      actors
+      director
+      writer
+      awards
+    }
+  }
+`;
 
 const ShowPage: NextPage = () => {
   const { query } = useRouter();
