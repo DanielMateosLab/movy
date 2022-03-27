120f2e6e5d200d840a13db056ae2cdf58d1e5b25
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-27 19:37:14 +0200

    Code clean

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -16,48 +16,26 @@ const resolvers: Resolvers = {
     title: ({ Title }) => Title,
     poster: ({ Poster }) => Poster,
     year: ({ Year }) => Year,
-    // This function avoids code repetition and also makes easy
-    // to implement new resolvers if we are interested in another
-    // field of the OMDb API.
-    ...generateParentOrAPIResolverForProperties(
-      "Genre",
-      "Plot",
-      "Director",
-      "Actors",
-      "Writer",
-      "Awards"
-    ),
+    genre: (parent, _, { dataSources }) => {
+      return dataSources.omdbApi.getPropertyFromParentOrAPI("Genre", parent);
+    },
+    plot: (parent, _, { dataSources }) => {
+      return dataSources.omdbApi.getPropertyFromParentOrAPI("Plot", parent);
+    },
+    director: (parent, _, { dataSources }) => {
+      return dataSources.omdbApi.getPropertyFromParentOrAPI("Director", parent);
+    },
+    actors: (parent, _, { dataSources }) => {
+      return dataSources.omdbApi.getPropertyFromParentOrAPI("Actors", parent);
+    },
+    writer: (parent, _, { dataSources }) => {
+      return dataSources.omdbApi.getPropertyFromParentOrAPI("Writer", parent);
+    },
+    awards: (parent, _, { dataSources }) => {
+      return dataSources.omdbApi.getPropertyFromParentOrAPI("Awards", parent);
+    },
   },
   Type: ShowType,
 };
-
-/** Helper function that generates, for each property given, a resolver that will:
- *
- * - Try to return the property from the parents arg.
- * - Call OMDbAPI rest API searching the show by id, and return the asked
- *     show property.
- *
- * Those resolvers will be returned in an object whose keys are the properties
- * in lower case, so they match our Show schema type.
- */
-function generateParentOrAPIResolverForProperties(
-  ...properties: Array<keyof ApiShow>
-) {
-  return properties.reduce(
-    (result, property) => ({
-      ...result,
-      // We return the property as lowercase to match our schema "Show"
-      [String(property).toLowerCase()]: generateParentOrAPIResolver(property),
-    }),
-    {}
-  );
-}
-
-/** For the given property, generates a resolver that uses OMDbAPI.getPropertyFromParentOrAPI() */
-function generateParentOrAPIResolver(property: keyof ApiShow) {
-  return (parent: ApiShow, _: undefined, { dataSources }: AppContextType) => {
-    return dataSources.omdbApi.getPropertyFromParentOrAPI(property, parent);
-  };
-}
 
 export default resolvers;
